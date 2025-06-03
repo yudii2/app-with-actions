e5ca7202cfc2f681880bd3d46225c8fa3a1f8f14
YUJIN KIM <84698468+yudii2@users.noreply.github.com>
2025-06-03 23:31:59 +0900

    Update ActionsController.java

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -8,7 +8,7 @@ public class ActionsController {
 	
 	@GetMapping(value = "/api/actions")
 	public String actionsTest() {
-		return "app-with-actions version:0.0.1";
+		return "app-with-actions version:0.0.2";
 	}
 	
 }
